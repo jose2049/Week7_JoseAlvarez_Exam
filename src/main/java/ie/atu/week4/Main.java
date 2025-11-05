6686d194efb668d86f4f67802cc1decd21c75bad
AlvarezJoseManuel-ST <g00463862@atu.ie>
2025-11-05 16:57:42 +0000

    Second Commits with class added and info gathered

--- src/main/java/ie/atu/week4/Main.java
+++ src/main/java/ie/atu/week4/Main.java
@@ -1,9 +1,62 @@
 package ie.atu.week4;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<CustomerClass>Customers=new ArrayList<CustomerClass>();
+        //print options
+        System.out.print("==== Customer Management System ====: ");
+        System.out.print("1) Add Customer ");
+        System.out.print("2) Update Customer ");
+        System.out.print("3) Display All ");
+        System.out.print("4) Exit ");
+        int opt=input.nextInt();
 
+        switch(opt){
+            case 1:
+                System.out.print("Enter the number of customers : ");
+                int n=input.nextInt();
+                int i = 0;
+                CustomerClass c=new CustomerClass();
+                while(i < n){
+
+                    System.out.print("Enter Customer Id : ");
+                    c.setCustomerID(new Scanner(System.in).nextLine());
+                    //Customers.add(c);
+                    System.out.print("Enter Customer Name : ");
+                    c.setName(new Scanner(System.in).nextLine());
+                    System.out.print("Enter Customer Email : ");
+                    c.setEmail(new Scanner(System.in).nextLine());
+                    System.out.print("Enter Customer Phone Number : ");
+                    c.setPhoneNumber(new Scanner(System.in).nextLine());
+                }
+                //add feature to prevent same info
+                break;
+                case 2:
+                    System.out.print("Enter Customer Id to update : ");
+                    for(CustomerClass cc : Customers){
+                        if(cc.getCustomerID().equals(input.nextLine())){
+                            System.out.print("Enter the update field (name, email, phone, id) : ");
+                            String update=input.nextLine();
+                            //to be continued
+                        }
+                    }
+                    break;
+            case 3:
+                //call function to display all data
+                break;
+            case 4:
+                System.out.print("You have exited the program\n");
+                break;
+            default:
+                System.out.print("Wrong choice");
+                break;
+        }
+        //create class
+        //create instances
     }
 }
